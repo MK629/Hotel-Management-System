12cb8fbdda9bbdf4d803f79c3266a456e80841ee
Artificial Idiot <myatkw629@gmail.com>
2025-03-11 19:46:51 +0630

    removed api route

--- Hotel-Frontend/hotel-app-user-side/src/app/api/token/route.js
+++
@@ -1,7 +0,0 @@
-import { getToken } from "@/services/credentialsService";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-    const token = await getToken()
-    return NextResponse.json({token})
-}
